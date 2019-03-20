adf40b9e7d04b4a99da6ac4d62dcb6e273d27a22
Joseph <z@charmed.tech>
2019-03-20 15:13:42 -0700

    Update solution to use binary method. Whiteboard.

--- challenges/arrayBinarySearch/__tests__/array-binary-search.test.js
+++ challenges/arrayBinarySearch/__tests__/array-binary-search.test.js
@@ -3,36 +3,30 @@
 const binarySearch = require('../array-binary-search.js');
 
 describe('`binarySearch` function', () => {
-  const arr = [0, 1, 2, 3, 1];
-  it('should return `null` if the first argument is not an array', () => {
+  const arr = [0, 1, 2, 3, 4];
+  it('should return `-1` if the first argument is not an array', () => {
     const str = 'str';
     const val = 0;
-    expect(binarySearch(str, val)).toBe(null);
+    expect(binarySearch(str, val)).toBe(-1);
   });
-  it('should return `null` if the second argument is an Object', () => {
+  it('should return `-1` if the second argument is an Object', () => {
     const val = {};
-    expect(binarySearch(arr, val)).toBe(null);
+    expect(binarySearch(arr, val)).toBe(-1);
   });
-  it('should return `null` if the second argument is an Array', () => {
+  it('should return `-1` if the second argument is an Array', () => {
     const val = ['val'];
-    expect(binarySearch(arr, val)).toBe(null);
+    expect(binarySearch(arr, val)).toBe(-1);
   });
-  it('should return `null` if the second argument is a Function', () => {
-    const val = function test() {
-      console.log('');
-    };
-    expect(binarySearch(arr, val)).toBe(null);
+  it('should return `-1` if the second argument is a Function', () => {
+    const val = function() {};
+    expect(binarySearch(arr, val)).toBe(-1);
   });
-  it('should return `null` if the second argument is undefined', () => {
-    expect(binarySearch(arr)).toBe(null);
+  it('should return `-1` if the second argument is undefined', () => {
+    expect(binarySearch(arr)).toBe(-1);
   });
   it('should return the index of the array element that is equal to the search key', () => {
     const val = 2;
     expect(binarySearch(arr, val)).toBe(2);
-  });
-  it('should return the index of only the first matching element', () => {
-    const val = 1;
-    expect(binarySearch(arr, val)).toBe(1);
   });
   it('should return -1 if the element is not in the array', () => {
     const val = 100;
--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,27 +1,19 @@
 'use strict';
 
-// return the index of the array's element
-// that is equal to the search key,
-// or -1 if the element does not exist
-
-// e.g., [4, 8, 15, 16, 23, 42], 15 -> 2
-// [11, 22, 33, 44, 55, 66, 77], 90 -> -1
-
 const binarySearch = (arr, val) => {
-  if (!val) {
-    return null;
-  }
-  if (typeof val !== 'string' && typeof val !== 'number') {
-    return null;
-  }
-  let result = -1;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === val) {
-      result = i;
-      break;
+  let low = 0;
+  let high = arr.length - 1;
+  while (high > low) {
+    let mid = Math.floor((high + low) / 2);
+    if (arr[mid] === val) {
+      return mid;
+    } else if (arr[mid] > val) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
     }
   }
-  return result;
+  return -1;
 };
 
 module.exports = binarySearch;
