95f4564f7d64b5acdcab8112b0409662a9bedbec
Joseph <z@charmed.tech>
2019-03-24 20:02:42 -0700

    Update LinkedList implementation and tests

--- data-structures/linkedList/__tests__/linked-list.test.js
+++ data-structures/linkedList/__tests__/linked-list.test.js
@@ -1,25 +1,51 @@
 'use strict';
 
+const { Node, LinkedList } = require('../linked-list.js');
+
 describe('LinkedList', () => {
   it('Can successfully instantiate an empty linked list', () => {
-    //
+    const ll = new LinkedList();
+    expect(ll).toBeDefined();
+    expect(ll.head).toBeNull();
   });
   it('Can properly insert into the linked list', () => {
-    //
+    const ll = new LinkedList();
+    ll.insert('a');
+    expect(ll.head).toBeInstanceOf(Node);
   });
   it('The head property will properly point to the first node in the linked list', () => {
-    //
+    const ll = new LinkedList();
+    ll.insert('a');
+    expect(ll.head.data).toBe('a');
   });
   it('Can properly insert multiple nodes into the linked list', () => {
-    //
+    const ll = new LinkedList();
+    ll.insert('a');
+    ll.insert('b');
+    ll.insert('c');
+    expect(ll.head.data).toBe('c');
+    expect(ll.head.next.data).toBe('b');
+    expect(ll.head.next.next.data).toBe('a');
   });
   it('Will return true when finding a value within the linked list that exists', () => {
-    //
+    const ll = new LinkedList();
+    ll.insert('a');
+    const bool = ll.includes('a');
+    expect(bool).toBeTruthy();
   });
   it('Will return false when searching for a value in the linked list that does not exist', () => {
-    //
+    const ll = new LinkedList();
+    ll.insert('a');
+    const bool = ll.includes('c');
+    expect(bool).toBeFalsy();
   });
   it('Can properly return a collection of all the values that exist in the linked list', () => {
-    //
+    const ll = new LinkedList();
+    ll.insert('a');
+    ll.insert('b');
+    ll.insert('c');
+    const collection = ll.print();
+    expect(collection).toBeDefined();
+    expect(collection).toEqual({ 0: 'c', 1: 'b', 2: 'a' });
   });
 });
--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -1,28 +1,44 @@
 'use strict';
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   // Within your LinkedList class, include a head property. Upon
   // instantiation, an empty Linked List should be created.
-  constructor(val) {
+  constructor() {
     // This object should be aware of a default empty value assigned to
     // `head` when the linked list is instantiated.
-    this.head = this.list[0];
-    this.list = [];
+    this.head = null;
+    this.size = 0;
   }
   // Define a method called `insert` which takes any value as an argument
   // and adds a new node with that value to the `head` of the list with
   // an O(1) Time performance.
   insert(val) {
-    this.list[this.list.length] = val;
+    const node = new Node(val); // create a new node
+    node.next = this.head; // assign the new node's pointer to the current head
+    this.head = node; // reassign the ll's head to the new node
+    this.size++; // increment the size of the ll
+    // N.B.: I read the instructions as adding a node to the head and updating the
+    // `head` flag, but to add something to the end, you'd just iterate through the nodes'
+    // `next` properties until you found `null`, then add your new node there with a `next`
+    // value of `null`
   }
   // Define a method called `includes` which takes any value as an argument
   // and returns a boolean result depending on whether that value exists as
   // a Node’s value somewhere within the list.
   includes(val) {
-    for (let i = 0; i < this.list.length; i++) {
-      if (this.list[i] === val) {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.data === val) {
         return true;
       }
+      currentNode = currentNode.next;
     }
     return false;
   }
@@ -30,8 +46,17 @@ class LinkedList {
   // Define a method called `print` which takes in no arguments and returns
   // a collection all of the current Node values in the Linked List.
   print() {
-    return this.list;
+    const collection = {}; // Collection will be an object with keys corresponding
+    // to the node's 0-based position in the linked list
+    let currentNode = this.head;
+    let i = 0;
+    while (currentNode !== null) {
+      collection[i] = currentNode.data;
+      i++;
+      currentNode = currentNode.next;
+    }
+    return collection;
   }
 }
 
-module.exports = LinkedList;
+module.exports = { Node, LinkedList };
