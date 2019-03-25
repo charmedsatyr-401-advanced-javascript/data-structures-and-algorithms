4124cf465fdc7d43cd0cfa3fcb67c8a2ae5dbe24
Joseph <z@charmed.tech>
2019-03-24 17:48:38 -0700

    LinkedList starter code

---
+++ data-structures/linkedList/__tests__/linked-list.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('LinkedList', () => {
+  it('Can successfully instantiate an empty linked list', () => {
+    //
+  });
+  it('Can properly insert into the linked list', () => {
+    //
+  });
+  it('The head property will properly point to the first node in the linked list', () => {
+    //
+  });
+  it('Can properly insert multiple nodes into the linked list', () => {
+    //
+  });
+  it('Will return true when finding a value within the linked list that exists', () => {
+    //
+  });
+  it('Will return false when searching for a value in the linked list that does not exist', () => {
+    //
+  });
+  it('Can properly return a collection of all the values that exist in the linked list', () => {
+    //
+  });
+});
