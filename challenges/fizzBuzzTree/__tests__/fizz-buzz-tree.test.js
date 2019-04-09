aa162ca7c89f480e1a0d4886f93f64051bbaf5ca
Joseph <z@charmed.tech>
2019-04-08 17:30:30 -0700

    FizzBuzzTree solution and tests

--- challenges/fizzBuzzTree/__tests__/fizz-buzz-tree.test.js
+++ challenges/fizzBuzzTree/__tests__/fizz-buzz-tree.test.js
@@ -4,13 +4,50 @@ const fizzBuzzTree = require('../fizz-buzz-tree.js');
 const { BinaryTree } = require('../../../data-structures/tree/tree.js');
 
 describe('`fizzBuzzTree` function', () => {
-  it('should return `null` if the argument `root` is `null`', () => {
+  it('should return `null` if the argument is not a tree', () => {
+    expect(fizzBuzzTree(0)).toBeNull();
+  });
+  it('should return an empty tree if the argument `root` is `null`', () => {
     const bt = new BinaryTree();
-    expect(fizzBuzzTree(bt)).toBeNull();
+    expect(fizzBuzzTree(bt)).toEqual(bt);
   });
   it('should replace the `root` value with the right word if it is divisible by 3, 5, or both', () => {
     const bt = new BinaryTree();
     bt.add(15);
     expect(fizzBuzzTree(bt).root.data).toBe('FizzBuzz');
+  });
+  it('should replace values divisible by `3` with `Fizz`', () => {
+    const bt = new BinaryTree();
+    bt.add(3);
+    bt.add(6);
+    bt.add(9);
+    bt.add(12);
+    bt.add(18);
+    bt.add(21);
+    bt.add(24);
+    const result = fizzBuzzTree(bt);
+    expect(result.root.data).toBe('Fizz');
+    expect(result.root.left.data).toBe('Fizz');
+    expect(result.root.right.data).toBe('Fizz');
+    expect(result.root.left.left.data).toBe('Fizz');
+    expect(result.root.left.right.data).toBe('Fizz');
+    expect(result.root.right.left.data).toBe('Fizz');
+    expect(result.root.right.right.data).toBe('Fizz');
+  });
+  it('should replace values divisible by `5` with `Buzz`', () => {
+    const bt = new BinaryTree();
+    bt.add(5);
+    bt.add(10);
+    bt.add(20);
+    bt.add(25);
+    bt.add(35);
+    bt.add(40);
+    const result = fizzBuzzTree(bt);
+    expect(result.root.data).toBe('Buzz');
+    expect(result.root.left.data).toBe('Buzz');
+    expect(result.root.right.data).toBe('Buzz');
+    expect(result.root.left.left.data).toBe('Buzz');
+    expect(result.root.left.right.data).toBe('Buzz');
+    expect(result.root.right.left.data).toBe('Buzz');
   });
 });
--- challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -1,23 +1,35 @@
 'use strict';
 
 const fizzBuzzTree = tree => {
-  let root = tree.root;
-  if (!root) {
+  if (typeof tree !== 'object') {
     return null;
   }
 
-  if (root.value % 3 === 0 && root.value % 5 === 0) {
-    root.value = 'FizzBuzz';
-  } else if (root.value % 3 === 0) {
-    root.value = 'Fizz';
-  } else if (root.value % 5 === 0) {
-    root.value = 'Buzz';
+  let current;
+  if (tree.root) {
+    current = tree.root;
+  } else if (tree.data) {
+    // If it's a subtree being evaluated recursively,
+    // it won't have a `root` property
+    current = tree;
+  } else {
+    // Return the argument if it doesn't have a root or data.
+    // Maybe it's a tree with an empty root?
+    return tree;
   }
-  if (root.left) {
-    this.fizzBuzzTree(root.left);
+  if (current.data % 3 === 0 && current.data % 5 === 0) {
+    current.data = 'FizzBuzz';
+  } else if (current.data % 3 === 0) {
+    current.data = 'Fizz';
+  } else if (current.data % 5 === 0) {
+    current.data = 'Buzz';
   }
-  if (root.right) {
-    this.fizzBuzzTree(root.right);
+
+  if (current.left) {
+    fizzBuzzTree(current.left);
+  }
+  if (current.right) {
+    fizzBuzzTree(current.right);
   }
   return tree;
 };
