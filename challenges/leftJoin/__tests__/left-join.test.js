7abfcfea144f03be428a49dcc3b1c1aee605dbbf
Joseph <z@charmed.tech>
2019-05-02 07:33:32 -0700

    Add nonfunctional implementation with broken tests

--- challenges/leftJoin/__tests__/left-join.test.js
+++ challenges/leftJoin/__tests__/left-join.test.js
@@ -1,60 +1,67 @@
 'use strict';
 
-// const { leftJoin } = require('../left-join.js');
+const { leftJoin } = require('../left-join.js');
 const { Hashtable } = require('../../../data-structures/hashtable/hashtable.js');
 
 describe('`leftJoin` function', () => {
-  it('should return an array containing items with values matching the values of keys shared by both arguments at those key indices', () => {
+  xit('should return an array containing items with values matching the values of keys shared by both arguments at those key indices', () => {
     const left = new Hashtable();
     left.add('fond', 'enamored');
     console.log('fond key:', left.hash('fond'));
     left.add('wrath', 'anger');
     console.log('wrath key:', left.hash('wrath'));
-    console.log('LEFLEFLELTEHLKGLHK:', left.table);
-    //    left.add('diligent', 'employed');
 
-    //   const right = new Hashtable();
-    // right.add('fond', 'averse');
-    //    right.add('wrath', 'delight');
-    //    right.add('diligent', 'idle');
-    //    right.add('guide', 'follow');
+    console.log('The full linked list:', left.table);
 
-    //  const result = leftJoin(left, right);
+    left.add('diligent', 'employed');
 
-    //  const fond = left.hash('fond');
-    //  // const wrath = left.hash('wrath');
-    //  // const diligent = left.hash('diligent');
-    //  const expected = ['enamored', 'averse'];
-    //  expect(result.table[fond].head.data).toEqual(expected);
-    // expect(result.table[wrath].head.data).toEqual(['anger', 'delight']);
-    // expect(result.table[diligent].head.data).toEqual(['employed', 'idle']);
+    const right = new Hashtable();
+    right.add('fond', 'averse');
+    right.add('wrath', 'delight');
+    right.add('diligent', 'idle');
+    right.add('guide', 'follow');
+
+    const result = leftJoin(left, right);
+
+    const fond = left.hash('fond');
+    const wrath = left.hash('wrath');
+    const diligent = left.hash('diligent');
+    const expected = ['enamored', 'averse'];
+
+    // These are broken because my Hashtable function adds every value to every linked list in its array.
+    expect(result.table[fond].head.data).toEqual(expected);
+    expect(result.table[wrath].head.data).toEqual(['anger', 'delight']);
+    expect(result.table[diligent].head.data).toEqual(['employed', 'idle']);
   });
-  //  xit('should add a `null` value for any keys right does not have in common with left', () => {
-  //    const left = new Hashtable();
-  //    left.add('fond', 'enamored');
-  //    left.add('wrath', 'anger');
-  //    left.add('diligent', 'employed');
-  //
-  //    const right = new Hashtable();
-  //    right.add('fond', 'averse');
-  //    right.add('wrath', 'delight');
-  //    right.add('diligent', 'idle');
-  //    right.add('guide', 'follow');
-  //
-  //    const result = leftJoin(left, right);
-  //
-  //    const fond = left.hash('fond');
-  //
-  //    console.log('ths ahsdkha;lkhgssdalkhsgadlh;aghlk;alh;asghl;', left.table[fond]);
-  //
-  //    const wrath = left.hash('wrath');
-  //    const diligent = left.hash('diligent');
-  //    // expect(result.table[fond].head.data).toEqual(['enamored', 'averse']);
-  //    // expect(result.table[wrath].head.data).toEqual(['anger', 'delight']);
-  //    // expect(result.table[diligent].head.data).toEqual(['employed', 'idle']);
-  //  });
-  //  xit('should not include items left does not have in common with right', () => {
-  //    //
-  //    //
-  //  });
+  xit('should add a `null` value for any keys right does not have in common with left', () => {
+    const left = new Hashtable();
+    left.add('fond', 'enamored');
+    left.add('wrath', 'anger');
+    left.add('diligent', 'employed');
+
+    const right = new Hashtable();
+    right.add('fond', 'averse');
+
+    const result = leftJoin(left, right);
+
+    const fond = left.hash('fond');
+
+    const wrath = left.hash('wrath');
+    const diligent = left.hash('diligent');
+    expect(result.table[fond].head.data).toEqual(['enamored', 'averse']);
+    expect(result.table[wrath].head.data).toEqual(['anger', null]);
+    expect(result.table[diligent].head.data).toEqual(['employed', null]);
+  });
+  xit('should not include items left does not have in common with right', () => {
+    const left = new Hashtable();
+    left.add('fond', 'enamored');
+
+    const right = new Hashtable();
+    right.add('wrath', 'anger');
+
+    const result = leftJoin(left, right);
+
+    const fond = left.hash('fond');
+    expect(result.table[fond].head.data).toEqual(['enamored', null]);
+  });
 });
--- challenges/leftJoin/left-join.js
+++ challenges/leftJoin/left-join.js
@@ -1,49 +1,40 @@
 'use strict';
 
-// const { Hashtable } = require('../../data-structures/hashtable/hashtable.js');
+const { Hashtable } = require('../../data-structures/hashtable/hashtable.js');
 
-// Assuming that both hash table parameters are instances of
-// `Hashtable`
+// Assuming that both hash table parameters are instances of `Hashtable`
 const leftJoin = (left, right) => {
-  console.log('here is left:', left.head);
-  console.log('here is right:', right.head);
-  // console.log('leftValues::::::::', left);
-  // const result = new Hashtable();
+  const result = new Hashtable();
   // Iterate through the hash table (which has a table
   // that is an array of linked lists)
-  //  for (let i = 0; i < left.table.length; i++) {
-  //    // Don't bother with empty linked lists.
-  //    if (left.table[i].head) {
-  //      //    console.log('does the shit have inifinite valus', left.table);
-  //      // Print the values in each linked list
-  //
-  //      const leftValues = left.table[i].print();
-  //      //     console.log('leftValues:::::::', i, '::', leftValues);
-  //      // We do not know what the original key was!
-  //      // i is just the index...
-  //      // Add the array as the value at that index of our hash table.
-  //      // (Must be insert, not append)
-  //      result.table[i].insert(leftValues);
-  //
-  //      // If the right hash table has something at that index
-  //      if (right.table[i].head) {
-  //        // Print it
-  //        // Even though we don't know if it was for the correct key
-  //        // or for a collision with that key...
-  //        // We are ignoring that problem.
-  //        const rightValues = right.table[i].print();
-  //        // Add them to the values of the result hash table
-  //        // at that index
-  //        result.table[i].head.data.push(...rightValues);
-  //      } else {
-  //        // Otherwise add `null` to the result hash table
-  //        // at that index
-  //        result.table[i].head.data.push(null);
-  //      }
-  //    }
-  //  }
-  //  // Return the result
-  //  return result;
+  for (let i = 0; i < left.table.length; i++) {
+    // Don't bother with empty linked lists.
+    if (left.table[i].head) {
+      // Print an array of values in each linked list
+      const leftValues = left.table[i].print();
+      // We do not know what the original key was, so i is just the index
+      // Add the array as the value at that index of our hash table.
+      // (Must be insert, not append)
+      result.table[i].insert(leftValues);
+
+      // If the right hash table has something at that index
+      if (right.table[i].head) {
+        // Print it even though we don't know if it was for the correct key
+        // or for a collision with that key...
+        // We are ignoring that problem.
+        const rightValues = right.table[i].print();
+        // Add them to the values of the result hash table
+        // at that index
+        result.table[i].head.data.push(...rightValues);
+      } else {
+        // Otherwise add `null` to the result hash table
+        // at that index
+        result.table[i].head.data.push(null);
+      }
+    }
+  }
+  // Return the result
+  return result;
 };
 
 module.exports = { leftJoin };
